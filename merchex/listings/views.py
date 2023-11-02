d91b922816b0f17ef574c7b8a3666d3a76991517
houda <houdacheikhahmed4@gmail.com>
2023-11-02 19:47:29 +0100

    creation des views

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def hello(request):
+    return HttpResponse('<h1>Hello Django!</h1>')
+def about(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+def list(request):
+    return HttpResponse('<h1>Voici la liste des annonces</h1> <p>Nous adorons palestine</p>')
+def contact(request):
+    return HttpResponse('<h1>Contact us</h1> <p>i love palestine !</p>')
+
